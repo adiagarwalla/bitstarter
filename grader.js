a6a388b412a51b95c00907ea4564fa0f0d8f0273
adiagarwalla <aa4@princeton.edu>
2013-07-09 07:07:21 +0000

    Working grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 var URL_DEFAULT = "http://protected-wildwood-4630.herokuapp.com/";
@@ -57,7 +58,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 };
 
 var checkHtmlFile1 = function(htmlfile, checksfile) {
-    $ = htmlfile;
+    $ = cheerio.load(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -78,20 +79,26 @@ if(require.main == module) {
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
 	.option('-u, --url <url>', 'URL to index.html')
-		.parse(process.argv);
+	.parse(process.argv);
 
     var checkJson;
 
-    if (program.url) {
+    if (program.url)
+    {
 	rest.get(program.url).on('complete', function(result) {
     checkJson = checkHtmlFile1(result, program.checks);
-	});
-    }
-else
-    checkJson = checkHtmlFile(program.file, program.checks);
-
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
-} else {
+	});
+    }
+
+    else
+    {
+	checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+}
+else {
     exports.checkHtmlFile = checkHtmlFile;
 }
